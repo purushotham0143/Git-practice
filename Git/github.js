c8b290afac47128f0e3d94f44877b56ea492f2d9
Ragala Purushotham <rpurushotham0143@gmail.com>
2024-04-07 15:43:46 +0530

    I am commiting git folder

---
+++ Git/github.js
@@ -0,0 +1,15 @@
+// Git and Github this two are not same
+// In this course we are learning about This Topics
+//1.vcs and git Reporaties
+//2.staging area and commit
+//3.Branches
+//4.Issues and PRS
+// Git->VCS->version Control System
+
+
+// IN git we have 4 parts
+//About this all Screanshorts in Laoptop
+// 1.is working Directory ->
+// 2.is staging area 
+// 3.local Reposatary
+// 4. is Remote reposatary
