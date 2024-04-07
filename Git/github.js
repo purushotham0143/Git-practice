eb03b7292c5ad12a3c24433fd6a4c05724eb36d8
Ragala Purushotham <rpurushotham0143@gmail.com>
2024-04-07 18:20:05 +0530

    i am commiting the changes

--- Git/github.js
+++ Git/github.js
@@ -1,3 +1,4 @@
+// This concept is from the vamsi bhavani chanell in utube
 // Git and Github this two are not same
 // In this course we are learning about This Topics
 //1.vcs and git Reporaties
@@ -13,3 +14,17 @@
 // 2.is staging area 
 // 3.local Reposatary
 // 4. is Remote reposatary
+
+//1.this is all about making and Initializing the project**********************
+// initialsize the project using  command ->                               git init
+//check ststus of the git using command ->                                 git status
+//then add the present directory to the staging command->                  git add file or folder
+//then from the statting to the local repotatory using command->           git commmit -m "Discription"
+//to create branches command->                                             git checkout -b "branch name"
+//then push from the local reporaties to remote reporatires command ->     git push -u "File or Foldername"
+
+
+
+//2.this all about the already the reporaties in the Remote Reporatory *******************
+//then step one fork the repotatry the the remote reporatory will come into the our local GitHub
+//then make the changes and send Pull request to the Remote repotatory Admin if it is ok then he will Mergr it 
